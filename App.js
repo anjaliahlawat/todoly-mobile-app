4d4ca47b6d77894c3af7dbb483902736b0ed18c6
anjaliahlawat16 <anjali@primesellerhub.com>
2021-03-18 19:36:55 +0530

    struture of app setup and welcome screen created

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <Text>Hi</Text>
+    <WelcomeScreen />
   );
 }
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ImageBackground, View, StyleSheet, Text } from 'react-native';
+
+import AppButton from '../components/Button/Button';
+import colors from '../config/colors';
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      blurRadius={1}
+      style={styles.background}
+      source={require('../assets/background.jpg')}
+    >
+      <View style={styles.logoContainer} blurRadius={2}>
+          <Text style={styles.tagline}>Declutter your mind</Text>      
+      </View>
+      <View style={styles.buttonContainer}>
+          <AppButton title="LOGIN" onPress={()=> console.log('Login')}/>
+          <AppButton title="REGISTER" onPress={()=> console.log('Register')} color="secondary"/>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background :{
+    flex : 1,
+    justifyContent: "flex-end",
+    alignItems : "center"
+  },
+  buttonContainer: {
+    padding : 20,
+    width : "100%",
+  },
+  logoContainer:{
+    alignItems:"center",
+    padding: 30,
+    position: 'absolute',
+    top: 100,
+  },
+  tagline:{
+    fontSize : 30,
+    fontWeight : "700",
+    color: colors.primary,
+    paddingVertical : 20
+  }
+})
+
+export default WelcomeScreen;
