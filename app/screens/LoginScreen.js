91fd504b30d8dc2317f072916a2dcc4c78b0f506
anjaliahlawat16 <anjali@primesellerhub.com>
2021-03-20 21:08:52 +0530

    login screen ui created

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import {AppForm, AppFormField, ErrorMessage, SubmitButton} from '../components/forms';
+import Screen from '../components/Screen';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password')
+})
 
 function LoginScreen(props) {
+  const [loginFailed, setLoginFailed] = useState(false)
+
+  const handleSubmit = async ({email, password}) => {
+    console.log(email, password)
+  }
+
   return (
-    <View style={styles.container}></View>
+    <Screen>
+        <ImageBackground 
+          blurRadius={1}
+          style={styles.background}
+          source={require('../assets/background.jpg')}
+        >
+        <AppForm
+          initialValues={{email: '', password: ''}}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}>
+            <ErrorMessage error="Invalid email and/or password." visible={loginFailed} />
+
+            <AppFormField 
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="email"
+                keyboardType="email-address"
+                name="email"
+                placeholder="Email"
+            />
+
+            <AppFormField 
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                name="password"
+                placeholder="Password"
+                secureTextEntry
+            />
+
+            <SubmitButton title="Login" />
+        </AppForm>
+        </ImageBackground>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{}
+  background:{
+    flex : 1,
+  },
+  logo:{
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20
+  }
 })
 
 export default LoginScreen;
